56ac77630f8da93e90c12bcdcd51992bce43d0a6
kghose <kaushik.ghose@sbgenomics.com>
2016-05-24 17:29:12 -0400

    d-plot improvement
    Went from heatmap to simple, overlaid, line plots. Reads better.

--- mitty/benchmarking/d_plot.py
+++ mitty/benchmarking/d_plot.py
@@ -32,13 +32,13 @@ logger = logging.getLogger(__name__)
 
 variant_categories = [
   ('100 < DEL', float('-inf'), -100),  # (key, i, j)  =>  i <= L < j
-  ('50 < DEL <= 100', -100, -50),
-  ('20 < DEL <= 50', -50, -20),
-  ('0 < DEL <= 20', -20, 0),
+  ('16 < DEL <= 100', -100, -16),
+  ('5 < DEL <= 16', -16, -5),
+  ('0 < DEL <= 5', -5, 0),
   ('SNP', 0, 1),
-  ('0 < INS <= 20', 1, 21),
-  ('20 < INS <= 50', 21, 51),
-  ('50 < INS <= 100', 51, 101),
+  ('0 < INS <= 5', 1, 6),
+  ('5 < INS <= 16', 5, 17),
+  ('16 < INS <= 100', 17, 101),
   ('100 < INS', 101, float('+inf')),
 ]
 
@@ -64,6 +64,45 @@ def cli(indelpkl, outsuffix, title):
 
 
 def plot_fig(read_counts, title):
+  plt.figure(figsize=(10, 5))
+  plt.subplots_adjust(left=0.1, bottom=0.1, top=0.95, right=0.98)
+  d_range = range(-creed.MAX_D_ERROR, creed.MAX_D_ERROR + 1)
+
+  indel_range = (read_counts['v_r_cnt'].shape[0] - 1 )/2
+  available_variant_categories = [
+    vc for vc in variant_categories
+    if (0 <= vc[1] + indel_range < 2 * indel_range + 1) or (0 < vc[2] + indel_range <= 2 * indel_range + 1)
+  ]
+
+  ref_y_xneg = read_counts['ref_r_cnt'][creed.MAX_D_ERROR::-1].cumsum()[::-1]
+  ref_y_xpos = read_counts['ref_r_cnt'][creed.MAX_D_ERROR:].cumsum()
+  # Need to duplicate d = 0 point for cumulative sum
+  # get rid of d = MAX_D + 1 which is unmapped and reads on wrong chrom
+  ref_y = np.concatenate((ref_y_xneg, ref_y_xpos[1:-1]))  # get rid of duplicate d = 0 point,
+  ref_pc = 100.0 * ref_y/max(1.0, ref_y_xneg[0] + ref_y_xpos[-1] - ref_y[creed.MAX_D_ERROR])
+
+  plt.plot(d_range, ref_pc, '.-', label='Ref')
+  plt.xlabel('d')
+  plt.ylabel('Bi-directional\ncumulative read %')
+  plt.suptitle(title)
+
+  y_xneg = read_counts['v_r_cnt'][:, creed.MAX_D_ERROR::-1].cumsum(axis=1)[:, ::-1]
+  y_xpos = read_counts['v_r_cnt'][:, creed.MAX_D_ERROR:].cumsum(axis=1)  # Need to duplicate d = 0 point for cumulative sum
+  y = np.concatenate((y_xneg, y_xpos[:, 1:-1]), axis=1)  # get rid of duplicate d = 0 point, get rid of d = MAX_D + 1 which is unmapped and reads on wrong chrom
+  Z = 100.0 * y/np.clip((y_xneg[:, 0] + y_xpos[:, -1] - y[:, creed.MAX_D_ERROR])[:, None], 1, 1e10)
+
+  for n, spec in enumerate(available_variant_categories):
+    i = max(0, spec[1] + indel_range)
+    j = max(0, min(spec[2] + indel_range, 2 * indel_range + 1))
+    plt.plot(d_range, Z[i:j, :].mean(axis=0), '.-', label=spec[0])
+
+  plt.legend(loc='lower right', fontsize=9)
+  ax = plt.gca()
+  ax.set_xlim([-20, 20])
+  ax.xaxis.grid(True)
+
+
+def plot_fig1(read_counts, title):
   indel_range = (read_counts['v_r_cnt'].shape[0] - 1 )/2
   d = range(-creed.MAX_D_ERROR, creed.MAX_D_ERROR + 1)
 
--- mitty/version.py
+++ mitty/version.py
@@ -1,1 +1,1 @@
-__version__ = '1.54.0dev0'
+__version__ = '1.54.1.dev0'
