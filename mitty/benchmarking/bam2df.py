476b2b69457936493297fe99bb6a24a6d80e361f
kghose <kaushik.ghose@sbgenomics.com>
2016-06-22 11:27:28 -0400

    Starting to integrate read/call association into code
    1. There is a commented section with a naive function, showing that it requires 1.5ms / call for a list of 80000 reads. This is too slow

--- mitty/benchmarking/bam2df.py
+++ mitty/benchmarking/bam2df.py
@@ -1,5 +1,10 @@
 """Read in an aligner (qname sorted) BAM and write out a CSV with pairs of reads with the aligned position,
-correct position and tags."""
+correct position and tags.
+
+
+python ~/Code/Mitty/mitty/benchmarking/bam2df.py qname-run00001.S.pe.100x500.perfect.g-G1.t-0.8.17.bam eval.df.csv read-call.csv -v
+
+"""
 import logging
 import time
 from multiprocessing import Pool
@@ -7,15 +12,20 @@ import subprocess
 
 import click
 import pysam
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
 read_info = [
   'd_error',   # Alignment error metric
   'a_mapped',  # Aligned mapped status
-  'a_pos',     # Aligned pos = chrom << 29 | pos
+  'a_p1',      # Aligned pos = chrom << 29 | pos
+  'a_p2',      # End pos of read
+  'a_cigar',
   'c_mapped',  # Correct mapped status
-  'c_pos',     # Correct pos
+  'c_p1',      # Correct pos
+  'c_p2',      # End pos of read
+  'c_cigar',
   'MQ'
 ]
 
@@ -38,13 +48,14 @@ gral_tags = [
 
 @click.command()
 @click.argument('qnamesortedbam')
+@click.argument('evalvcfdf')
 @click.argument('outcsv')
 @click.option('--paired-reads/--single-end-reads', default=True, help='Are these paired end or single end reads')
 @click.option('--simulated-reads/--real-reads', default=True, help='Are these simulated from Mitty? If so, parse qnames for truth positions')
 @click.option('--block-size', default=100000, help='Block size')
 @click.option('-t', default=4, help='Threads')
 @click.option('-v', count=True, help='Verbosity level')
-def cli(qnamesortedbam, outcsv, paired_reads, simulated_reads, block_size, t, v):
+def cli(qnamesortedbam, evalvcfdf, outcsv, paired_reads, simulated_reads, block_size, t, v):
   """Associate reads in a BAM with variant calls."""
   level = logging.DEBUG if v > 0 else logging.WARNING
   logging.basicConfig(level=level)
@@ -52,11 +63,15 @@ def cli(qnamesortedbam, outcsv, paired_reads, simulated_reads, block_size, t, v)
   logger.warning('This code only works for paired end simulated files ...')
   logger.warning('It is trvial to convert it to work for data with no qname information and SE')
 
-  process_bam_parallel(qnamesortedbam, outcsv, block_size, threads=t)
+  process_bam_parallel(qnamesortedbam, evalvcfdf, outcsv, block_size, threads=t)
 
 
-def process_bam_parallel(qnamesortedbam, out_csv, block_size=10000, threads=4):
+def process_bam_parallel(qnamesortedbam, evalvcfdf, out_csv, block_size=10000, threads=4):
   """Header is in 'tmp_hdr.csv' partials are in tmp_1.csv, tmp_2.csv ...."""
+
+  #eval_df = load_eval_df(qnamesortedbam, evalvcfdf)
+  eval_df = []
+
   prefix = 'temp_bam2df'
 
   columns = ['qname'] + [m + t for m in ['m1_', 'm2_'] for t in read_info + gral_tags]
@@ -71,7 +86,7 @@ def process_bam_parallel(qnamesortedbam, out_csv, block_size=10000, threads=4):
   p = Pool(threads)
   t_total = 0
   t0 = time.time()
-  for fn in p.imap_unordered(process_bam_section, [(qnamesortedbam, off, fn, block_size) for off, fn in zip(offsets, fnames)]):
+  for fn in p.imap_unordered(process_bam_section, [(qnamesortedbam, off, fn, eval_df, block_size) for off, fn in zip(offsets, fnames)]):
     # This will only work on unix like systems
     with open(out_csv, 'a') as fp:
       subprocess.call(['cat', fn], stdout=fp)
@@ -84,6 +99,20 @@ def process_bam_parallel(qnamesortedbam, out_csv, block_size=10000, threads=4):
   subprocess.call(['gzip', '-f', out_csv])
 
 
+def load_eval_df(qnamesortedbam, evalvcfdf):
+  eval_df = pd.read_csv(evalvcfdf, dtype={'chrom': 'S10'}, compression='gzip' if evalvcfdf.endswith('gz') else None)
+  seq_dict = seq_dict_from_bam(qnamesortedbam)
+  eval_df['temp_chrom'] = eval_df.apply(lambda row: seq_dict[row['chrom']], axis=1)
+  eval_df['v_p1'] = eval_df.apply(lambda row: (row['temp_chrom'] << 29) | row['pos'], axis=1)
+  eval_df['v_p2'] = eval_df.apply(lambda row: (row['temp_chrom'] << 29) | row['pos_stop'], axis=1)
+  return eval_df
+
+
+def seq_dict_from_bam(qnamesortedbam):
+  fp = pysam.AlignmentFile(qnamesortedbam)
+  return {s['SN']: n + 1 for n, s in enumerate(fp.header['SQ'])}
+
+
 def break_bam(qnamesortedbam, block_size):
   """Go through the BAM and mark-off the offsets,
 
@@ -91,7 +120,6 @@ def break_bam(qnamesortedbam, block_size):
   :param block_size:
   :return:
   """
-  t_total = 0
   t0 = time.time()
   offsets = [off for off in get_bam_sections(qnamesortedbam, block_size)]
   t1 = time.time()
@@ -106,6 +134,9 @@ def get_bam_sections(qnamesortedbam, block_size):
     next(fp)
     if cnt % block_size == 0:
       yield fp.tell()
+
+    if cnt == 10000:
+      break
 
 
 # https://groups.google.com/forum/#!topic/pysam-user-group/bBdqn7DkVtE
@@ -122,7 +153,7 @@ def get_bam_sections(qnamesortedbam, block_size):
 
 # args = qnamesortedbam, template_start, template_end
 def process_bam_section(args):
-  qnamesortedbam, offset, fname, block_size = args
+  qnamesortedbam, offset, fname, eval_df, block_size = args
   fp = pysam.AlignmentFile(qnamesortedbam)
   fp.seek(offset)
 
@@ -208,16 +239,54 @@ def parse_qname(read):
     'qname': read.qname,
     'mate': 0 if read.is_read1 else 1,
     'a_mapped': a_mapped,
-    'a_pos': a_pos,
+    'a_p1': a_pos,
+    'a_p2': a_pos + read.rlen,
     'a_cigar': a_cigar,
     'c_mapped': c_mapped,
-    'c_pos': c_pos,
+    'c_p1': c_pos,
+    'c_p2': c_pos + read.rlen,
     'c_cigar': cigar,
     'MQ': read.mapping_quality,
   }
   for k, v in read.get_tags():
     r_dict[k] = v
   return r_dict
+
+
+# # The naive searching n^2 method ->
+# # With 80000 templates
+# # %timeit get_templates_over_call(bam_df, call)
+# # 100 loops, best of 3: 5.52 ms per loop
+# # -> 30e6 templates -> 5.52e-3 * 30e6 * 2e5 / 80000 -> too long even with our medium sized data set
+#
+# def get_templates_over_call(bam_df, call):
+#   """Given a chunk of a BAMdf find all the reads under a given variant"""
+#
+#   call_read_list = []
+#
+#   v_p1 = call['v_p1']
+#   v_p2 = call['v_p2']
+#
+#   # All the mate1 reads that are aligned underneath the call
+#   m1_aligned = (bam_df['m1_a_p1'] < v_p2) & (bam_df['m1_a_p2'] > v_p1)
+#
+#   # All the mate2 reads that are aligned underneath the call
+#   m2_aligned = (bam_df['m2_a_p1'] < v_p2) & (bam_df['m2_a_p2'] > v_p1)
+#
+#   # All the mate1 reads that should be aligned underneath the call
+#   m1_correct = (bam_df['m1_c_p1'] < v_p2) & (bam_df['m1_c_p2'] > v_p1)
+#
+#   # All the mate2 reads that are aligned underneath the call
+#   m2_correct = (bam_df['m2_c_p1'] < v_p2) & (bam_df['m2_c_p2'] > v_p1)
+#
+#   citems = call.to_dict().items()
+#   for r in bam_df[m1_aligned & ~m2_aligned].iterrows():
+#     call_read_list.append(
+#       {k: v for k, v in citems + r.items() + [('m1_aligned_under_feature', 1), ('m2_aligned_under_feature', 0)]}
+#     )
+#
+#   return call_read_list
+
 
 if __name__ == '__main__':
   cli()
